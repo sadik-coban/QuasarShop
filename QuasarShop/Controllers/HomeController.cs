f8c09ea0c55a3862dc17c23b270950cc4913873b
amonrabravo <onay.yalciner@hotmail.com>
2023-12-09 10:20:07 +0300

    migrations

--- QuasarShop/Controllers/HomeController.cs
+++ QuasarShop/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+
     }
 
     public IActionResult Index()
--- QuasarShop/Program.cs
+++ QuasarShop/Program.cs
@@ -1,9 +1,31 @@
+using Microsoft.EntityFrameworkCore;
 using QuasarShopData;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder
+    .Services
+    .AddDbContext<AppDbContext>(config =>
+    {
+        var dbProvider = builder.Configuration.GetValue<string>("DbProvider");
+        var cs = builder.Configuration.GetConnectionString(dbProvider!);
+        switch (dbProvider)
+        {
+            case "PostgreSql":
+                config.UseNpgsql(cs, p => p.MigrationsAssembly("MigrationsPostgreSql"));
+                break;
+            case "MySql":
+                config.UseMySql(cs, ServerVersion.AutoDetect(cs), p => p.MigrationsAssembly("MigrationsMySql"));
+                break;
+            case "SqlServer":
+            default:
+                config.UseSqlServer(cs, p => p.MigrationsAssembly("MigrationsSqlServer"));
+                break;
+        }
+    });
 
 var app = builder.Build();
 
